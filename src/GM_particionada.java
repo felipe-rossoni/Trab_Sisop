cd9ec51c1fe359e5d13a6dd7043f9d34875d8672
“Thiago <thiago_muller@outlook.com>
2023-10-01 12:21:37 -0300

    add GM particao fix

---
+++ src/GM_particionada.java
@@ -0,0 +1,42 @@
+public class GM_particionada {
+    private int tamPart;
+    private int tamMem;
+    private Word[] m;   // Acesso direto a memória
+    private boolean[] particao;
+
+    public GM_particionada(Memory mem, int tamPart){
+        this.tamMem = mem.tamMem;
+        this.tamPart = tamPart;
+        this.m = mem.m;
+
+        // Define o número de partições
+        if(tamMem % tamPart > 0)
+            this.particao = new boolean[tamMem/tamPart + 1];
+        else
+            this.particao = new boolean[tamMem/tamPart];
+    }
+
+    public int get_tamPart() {
+        return tamPart;
+    }
+
+    public int aloca() {
+        // Encontra a primeira partição livre e a aloca
+        for (int i = 0; i < particao.length; i++) {
+            if (!particao[i]) {
+                particao[i] = true;
+                return i;
+            }
+        }
+        System.out.println("Nenhuma partição disponível!");
+        return -1;
+    }
+
+    public void desaloca(int part) {
+        // Libera a partição especificada
+        if (part >= 0 && part < particao.length) {
+            particao[part] = false;
+            for (int i=part*tamPart; i<part*tamPart + tamPart; i++) { m[i] = new Word(Opcode.___,-1,-1,-1); };
+        }
+    }
+}
