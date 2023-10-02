a3105106a766e6b5e942c669a5d403df5442bd0c
Thiago Müller <101720947+ThiagoMllr@users.noreply.github.com>
2023-10-01 22:56:16 -0300

    Add files via upload

---
+++ PCB.java
@@ -0,0 +1,28 @@
+public class PCB {
+    private int id;
+    private int pc;
+    private int[] reg;
+    private int[] paginas;
+    private int partUsada;
+
+    public PCB(int id, int pc, int[] paginas){
+        this.id = id;
+        this.pc = pc;
+        this.paginas = paginas;
+    }
+
+    public PCB(int id, int pc, int particao){
+        this.id = id;
+        this.pc = pc;
+        this.partUsada = particao;
+    }
+
+    public int[] getPaginas() {
+        return paginas;
+    }
+
+    public int getPartUsada() {
+        return partUsada;
+    }
+
+}
